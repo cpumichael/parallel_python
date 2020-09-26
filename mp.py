ed49da91467457b9132e59f2d0a267024f4c3933
Michael Barnes <mbarnes@Michaels-MacBook-Pro.local>
2020-09-25 21:34:34 -0700

    added getpid() and getppid()

--- mp.py
+++ mp.py
@@ -1,9 +1,11 @@
 from multiprocessing import Process
+import os
 
 def f(name, x):
-    print('hello', name, x)
+    print('I am child: hello', name, x, 'pid=', os.getpid(), 'ppid=', os.getppid())
 
 if __name__ == '__main__':
+    print('PARENT pid=', os.getpid(), 'ppid=', os.getppid())
     p = Process(target=f, args=('bob', 'rabbit'))
     p.start()
     p.join()
